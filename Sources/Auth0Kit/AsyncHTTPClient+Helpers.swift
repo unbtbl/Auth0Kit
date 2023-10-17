776e024145cbfbb0ff25a674a64fd3a56529e132
Joannis Orlandos <joannis@orlandos.nl>
2023-10-17 18:12:14 +0200

    Add get roles, add permissions and delete user

--- Sources/Auth0Kit/AsyncHTTPClient+Helpers.swift
+++ Sources/Auth0Kit/AsyncHTTPClient+Helpers.swift
@@ -1,14 +1,22 @@
 import NIOHTTP1
 import AsyncHTTPClient
+import Foundation
 
 struct HTTPError: Error {
     let code: HTTPResponseStatus
 }
 
 extension HTTPClientResponse {
-    func assertSuccessful() throws {
+    func assertSuccessful() async throws {
         guard status.code >= 200 && status.code < 300 else {
             throw HTTPError(code: status)
         }
+    }
+
+    func decode<D: Decodable>(as type: D.Type = D.self) async throws -> D {
+        let body = try await self.body.collect(upTo: Int(UInt16.max))
+        return try JSONDecoder
+            .custom(keys: .convertFromSnakeCase)
+            .decode(D.self, from: body)
     }
 }
--- Sources/Auth0Kit/Management.swift
+++ Sources/Auth0Kit/Management.swift
@@ -1,6 +1,6 @@
-import Vapor
-import Foundation
 import AsyncHTTPClient
+import Foundation
+import Vapor
 
 extension Auth0 {
     public struct Management {
@@ -12,20 +12,108 @@ extension Auth0 {
             var request = HTTPClientRequest(url: "\(auth0.issuer)oauth/token")
             request.method = .POST
             request.headers.contentType = .json
-            request.body = try .bytes(JSONEncoder().encodeAsByteBuffer([
-                "grant_type": "client_credentials",
-                "client_id": clientId,
-                "client_secret": clientSecret,
-                "audience": auth0.audience
-            ], allocator: auth0.allocator))
+            request.body = try .bytes(
+                JSONEncoder().encodeAsByteBuffer(
+                    [
+                        "grant_type": "client_credentials",
+                        "client_id": clientId,
+                        "client_secret": clientSecret,
+                        "audience": auth0.issuer + "api/v2/",
+                    ],
+                    allocator: auth0.allocator
+                )
+            )
             let response = try await auth0.client.execute(request, timeout: .seconds(15))
-            try response.assertSuccessful()
-            let body = try await response.body.collect(upTo: Int(UInt16.max))
+            try await response.assertSuccessful()
 
-            return try JSONDecoder
-                .custom(keys: .convertFromSnakeCase)
-                .decode(Auth0TokenResponse.self, from: body)
-                .accessToken
+            return try await response.decode(as: Auth0TokenResponse.self).accessToken
+        }
+
+        public func deleteUser(byId id: String) async throws {
+            let token = try await getToken()
+            var request = HTTPClientRequest(
+                url:
+                    "\(auth0.issuer)api/v2/users/\(id.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? id)"
+            )
+            request.method = .DELETE
+            request.headers.bearerAuthorization = .init(token: token)
+
+            let response = try await auth0.client.execute(request, timeout: .seconds(15))
+            try await response.assertSuccessful()
+        }
+
+        public func createRole(named name: String, description: String) async throws -> Auth0Role {
+            let response = try await send(
+                .POST,
+                to: "roles",
+                body: [
+                    "name": name,
+                    "description": description,
+                ]
+            )
+
+            return try await response.decode(as: Auth0Role.self)
+        }
+
+        public func getRoles() async throws -> [Auth0Role] {
+            let token = try await getToken()
+            var request = HTTPClientRequest(url: "\(auth0.issuer)api/v2/roles")
+            request.method = .GET
+            request.headers.bearerAuthorization = .init(token: token)
+            let response = try await auth0.client.execute(request, timeout: .seconds(15))
+            try await response.assertSuccessful()
+
+            do {
+                return try await response.decode(as: [Auth0Role].self)
+            } catch {
+                return try await response.decode(as: Roles.self).roles
+            }
+        }
+
+        public func addScopes(_ scopes: [Scope], toAPI id: String) async throws {
+            try await send(
+                .PATCH,
+                to: "resource-servers/\(id)",
+                body: [
+                    "scopes": scopes
+                ]
+            )
+        }
+
+        public func addPermissions(
+            _ permissions: [Permission],
+            toRole roleId: String
+        ) async throws {
+            try await send(
+                .POST,
+                to: "roles/\(roleId)/permissions",
+                body: [
+                    "permissions": permissions
+                ]
+            )
+        }
+
+        @discardableResult
+        private func send<E: Encodable>(
+            _ method: HTTPMethod,
+            to path: String,
+            body: E
+        ) async throws -> HTTPClientResponse {
+            let token = try await getToken()
+            var request = HTTPClientRequest(url: "\(auth0.issuer)api/v2/\(path)")
+            request.method = method
+            request.headers.bearerAuthorization = .init(token: token)
+            request.headers.contentType = .json
+            request.body = .bytes(
+                try JSONEncoder.custom(keys: .convertToSnakeCase).encodeAsByteBuffer(
+                    body,
+                    allocator: auth0.allocator
+                )
+            )
+
+            let response = try await auth0.client.execute(request, timeout: .seconds(15))
+            try await response.assertSuccessful()
+            return response
         }
     }
 
@@ -34,6 +122,36 @@ extension Auth0 {
     }
 }
 
+public struct Scope: Codable {
+    public let value: String
+    public let description: String
+
+    public init(value: String, description: String) {
+        self.value = value
+        self.description = description
+    }
+}
+
+public struct Permission: Codable {
+    public var resourceServerIdentifier: String
+    public var permissionName: String
+
+    public init(resourceServerIdentifier: String, permissionName: String) {
+        self.resourceServerIdentifier = resourceServerIdentifier
+        self.permissionName = permissionName
+    }
+}
+
 struct Auth0TokenResponse: Codable {
     let accessToken: String
+}
+
+public struct Auth0Role: Codable {
+    public let id: String
+    public let name: String
+    public let description: String
+}
+
+struct Roles: Codable {
+    let roles: [Auth0Role]
 }
